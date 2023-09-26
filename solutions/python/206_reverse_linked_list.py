c3a1cfbcc08de292044cf98dec9dce7f28472ef1
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2023-09-26 16:58:45 +0200

    feat(206_reverse_linkedin_list.py): solved 206 reverse linked list (beats 91% in runtime)

---
+++ solutions/python/206_reverse_linked_list.py
@@ -0,0 +1,29 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev_node = None
+        curr_node = head
+        while curr_node:
+            next_node = curr_node.next
+            curr_node.next = prev_node
+            prev_node = curr_node
+            curr_node = next_node
+        return prev_node
+
+
+node5 = ListNode(5)
+node4 = ListNode(4, node5)
+node3 = ListNode(3, node4)
+node2 = ListNode(2, node3)
+node1 = ListNode(1, node2)
+
+head = Solution().reverseList(node1)
